8b0032a969be90957d61dd2ce712280cb509e526
mydzinx <xcentek@gmail.com>
2017-06-28 23:00:31 +0200

    0.1.0
    Projekt do otworow H5

---
+++ otwor.h
@@ -0,0 +1,19 @@
+#ifndef OTWOR_H
+#define OTWOR_H
+#include "mainwindow.h"
+
+class otwor
+{
+public:
+    otwor();
+    void sprawdzanie_otworu_A();
+    void sprawdzanie_otworu_B();
+    void sprawdzanie_otworu_C();
+    void sprawdzanie_otworu_D();
+    void sprawdzanie_otworu_E();
+    void sprawdzanie_otworu_F();
+    void sprawdzanie_otworu_G();
+    void sprawdzanie_otworu_H();
+};
+
+#endif // OTWOR_H
